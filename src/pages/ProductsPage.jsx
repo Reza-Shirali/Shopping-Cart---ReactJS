6d208d201255a77330b27248859080e596fca89d
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-10-18 13:10:47 +0330

    create search and category section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../context/ProductContext";
 import Card from "../components/Card";
 import PulseLoader from "react-spinners/PulseLoader";
 
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
 const ProductsPage = () => {
   const products = useProducts();
+
+  const [displayed, setDisplayed] = useState([]);
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  const searchHandler = () => {
+    console.log("search");
+  };
+
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+
+    if (tagName !== "LI") return;
+  };
   return (
-    <div className="flex justify-between">
-      <div className="w-full flex flex-wrap justify-between">
-      {!products.length ? <PulseLoader color="#fe5d42" className="w-full my-0 mx-auto flex justify-center text-center h-[100rem] mt-[10rem]" />  : null}
-        {products.map((product) => {
-          return <Card key={product.id} data={product} />;
-        })}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className="flex justify-between">
+        <div className="w-full flex flex-wrap justify-between">
+          {!displayed.length ? (
+            <PulseLoader
+              color="#fe5d42"
+              className="w-full my-0 mx-auto flex justify-center text-center h-[100rem] mt-[10rem]"
+            />
+          ) : null}
+          {displayed.map((product) => {
+            return <Card key={product.id} data={product} />;
+          })}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelry</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
