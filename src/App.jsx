645dcc82f64500866e3932fbc3d2c017385f15a1
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-10-18 11:33:53 +0330

    Routing Process

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckOutPage from "./pages/CheckOutPage";
+import PageNotFound from "./pages/404"
 
 function App() {
   return (
-   <>
-    
-   </>
-  )
+    <>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
